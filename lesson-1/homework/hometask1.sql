27a678bb8b87c1dab8cb3bb614b9c83c33a2166a
ddilshodov1 <126846397+DilshodovDilmurod@users.noreply.github.com>
2025-08-28 10:40:54 +0500

    Update hometask1.sql

--- lesson-1/homework/hometask1.sql
+++ lesson-1/homework/hometask1.sql
@@ -1,2 +1,22 @@
 
-11
+1. Data - This is any information 
+DAtabase  is  a place  to store data
+Relational database - ozaro boglangan database 
+table - rows and columns 
+2. data query language(DQL), data definition language(DDL), data manipulation language(DML), data control language(DCL), transaction control language(TCL)
+3.-
+4.create database SchoolDB
+use SchoolDB
+go
+5. create table Students (StudentID int primary key, name varchar(50), age int)
+6. sql - it is programming language for writing queries.
+sql server - this save database informafion and run sql commands
+ssms - that is enviorements for coders
+7.DQL - this is  to writing for query and select 
+DDL - this is to change database structure
+DML - this is to change data structure
+DCL - this is give or remove access for any users
+TCL - try a transuction 
+8. insert into Students values (1, 'Anvar', 21), (2, 'Abror', 22), (3, 'Jalol', 32)
+select StudentID, name, age from Students
+9. done
