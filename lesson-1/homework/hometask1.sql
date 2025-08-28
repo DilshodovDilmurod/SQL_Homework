a93347ef63fd66af8b1c6ef76ac13ce714d938fe
ddilshodov1 <126846397+DilshodovDilmurod@users.noreply.github.com>
2025-08-28 15:08:43 +0500

    Update hometask1.sql

--- lesson-1/homework/hometask1.sql
+++ lesson-1/homework/hometask1.sql
@@ -1,9 +1,9 @@
 
 1. Data - This is any information 
 DAtabase  is  a place  to store data
-Relational database - ozaro boglangan database 
+Relational database - relationship database 
 table - rows and columns 
-2. Malumotlar xavfsizligi, zaxiralash, qayta tiklay olish, integratsiya, reporting
+2. Data security, backup, restore, integretion, reporting
 3.-windows and sql server
 4.create database SchoolDB
 use SchoolDB
@@ -13,10 +13,28 @@ go
 sql server - this save database informafion and run sql commands
 ssms - that is enviorements for coders
 7.DQL - this is  to writing for query and select 
+  select is used to get data from tables 
+  query- function
 DDL - this is to change database structure
+  truncate- to remove all data from table
+  create- to create a table
+  alter- to change table structure
+  drop - to remove a table
 DML - this is to change data structure
+  insert - to enter data to a table
+  apdate - to change data from a table
+  delete - to delete data from a table
 DCL - this is give or remove access for any users
+  grand - to give access to a users
+  revoke - to remove access from a users
 TCL - try a transuction 
+  begin - to strar a transuction
+  commit - to save your change
+  roolback - to cancel your change
 8. insert into Students values (1, 'Anvar', 21), (2, 'Abror', 22), (3, 'Jalol', 32)
 select StudentID, name, age from Students
-9. done
+9. 
+enter the link and dowland the file 
+file copy or cut and go to set ssms file and enter  MSSQL16.MSSQLSERVER
+then MSSQl ->backup finally pass dowloand  file 
+and find the file link from SSMS
